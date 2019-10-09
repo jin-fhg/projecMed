63bc75ad39cd91844836be0c6eab6ba66465cb99
jin-fhg <jinphoenix58@gmail.com>
2019-10-09 08:42:05 +0800

    Added new email cred  for account activation

--- projectMed/settings.py
+++ projectMed/settings.py
@@ -142,8 +142,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'djangotest10001@gmail.com'
-EMAIL_HOST_PASSWORD = '@Rc12013980410284'
+EMAIL_HOST_USER = 'djangotestersseertimes@gmail.com'
+EMAIL_HOST_PASSWORD = 'seertimes12345'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 
